c3fc4a161f3fdb391c1f499448b4ffaefa728fb2
adityaprast07-eng <adityaprast07@gmail.com>
2026-07-06 19:54:58 +0700

    Menambahkan tugas Minggu 14 dan Minggu 15

---
+++ src/Minggu_15/Perpustakaan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Minggu_15;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Perpustakaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                new kelolajenisbuku().show();
+    }
+    
+}
